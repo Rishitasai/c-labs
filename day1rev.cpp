34f5824ef3d458aca7dc7fa9a0d11bbed7e1aac8
Rishitasai <112751961+Rishitasai@users.noreply.github.com>
2022-10-06 09:14:04 +0530

    Add files via upload

---
+++ day1rev.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+int main()
+{
+    char str[100];//declare a character array
+    int i,len,temp;
+    cout<<"Enter a String as you wish\n";
+    cin>>str; //input string
+    len=strlen(str);
+    i=0;
+    while(i<len/2){
+        temp=str[i];
+        str[i]=str[len-i-1];
+        str[len-i-1]=temp;
+ i++;
+    }
+   cout<<"Given String is reversed here\n"<<str;
+   return 0;
+}
